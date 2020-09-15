ab9679994beea04f89ed7087b04e18983b6d0683
jallend1 <jallend1@gmail.com>
2020-09-14 20:53:44 -0700

    Adds MapboxGL and begins creating a map for each location

--- src/Components/Brewery.js
+++ src/Components/Brewery.js
@@ -1,38 +1,56 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import mapboxgl from "mapbox-gl";
 
-const Brewery = (props) => {
-  console.log(props);
-  const id = useParams();
-  const brewery = props.location.state;
-  console.log(props.location.state);
-  return (
-    <>
-      <p>brewery page</p>
-      <div className="card">
-        <div className="card-title center">{brewery.name}</div>
-        <div className="card-content center">
-          <div className="location">
+class Brewery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mapContainer = React.createRef();
+  }
+  componentDidMount() {
+    mapboxgl.accessToken = "";
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [0, 0],
+      zoom: 2,
+    });
+  }
+
+  render() {
+    const id = useParams();
+    const brewery = this.props.location.state;
+    console.log(this.props.location.state);
+    return (
+      <>
+        <p>brewery page</p>
+        <div className="card">
+          <div className="card-title center">{brewery.name}</div>
+          <div className="card-content center">
+            <div className="location">
+              <p>
+                <strong>Location:</strong> {brewery.city}, {brewery.state}
+              </p>
+              <p>
+                <strong>Street address:</strong> {brewery.street}
+              </p>
+              <p>
+                <strong>ZIP Code:</strong> {brewery.postal_code}
+              </p>
+            </div>
+            <a href={brewery.website_url}>{brewery.website_url}</a>
             <p>
-              <strong>Location:</strong> {brewery.city}, {brewery.state}
+              <strong>Phone Number:</strong>
+              {brewery.phone}
             </p>
-            <p>
-              <strong>Street address:</strong> {brewery.street}
-            </p>
-            <p>
-              <strong>ZIP Code:</strong> {brewery.postal_code}
-            </p>
+            <p>Brewery ID: {brewery.id}</p>
+            <div>
+              <div ref={(el) => (this.mapContainer = el)} />
+            </div>
           </div>
-          <a href={brewery.website_url}>{brewery.website_url}</a>
-          <p>
-            <strong>Phone Number:</strong>
-            {brewery.phone}
-          </p>
-          <p>Brewery ID: {brewery.id}</p>
         </div>
-      </div>
-    </>
-  );
-};
-
+      </>
+    );
+  }
+}
 export default Brewery;
