46f881fec161f4ac645b3e11f9469ee9af715ea3
jallend1 <jallend1@gmail.com>
2020-09-30 12:16:31 -0700

    Extracts MapGL token into isolated file so I don't keep accidentally exposing my private business on GitHub

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
+import { token } from '../token';
 
 class Map extends React.Component {
   state = {
@@ -15,7 +16,7 @@ class Map extends React.Component {
     return (
       <ReactMapGL
         {...this.state.viewport}
-        mapboxApiAccessToken="pk.eyJ1IjoiamFsbGVuZDEiLCJhIjoiY2tmM2Nld3hvMDFxbzMwbW4yNGk2c3NybiJ9.n6nnx-xBKBI_1jxp3fxZaA"
+        mapboxApiAccessToken={token}
         onViewportChange={(viewport) => this.setState({ viewport })}
       >
         <Marker
